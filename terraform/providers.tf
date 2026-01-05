04a2b6334b55d734b7c6535440bc3ba6364f8e43
Suyash Gandhi <94231954+Suyash0201@users.noreply.github.com>
2026-01-05 15:52:18 +0530

    cccc

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,6 @@
-provider "aws" {
-  region = var.region
-}
-
 provider "azurerm" {
   features {}
 
-  # Use Service Principal authentication instead of Azure CLI
   subscription_id = "<your-subscription-id>"
   client_id       = "<your-service-principal-app-id>"
   client_secret   = "<your-service-principal-password>"
